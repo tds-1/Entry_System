fe66a021672cf4cb0a2956ef4cd843dfa8b78123
tds-1 <tanmaydeepsharma21@gmail.com>
2019-12-01 17:40:54 +0530

    app changes made

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 config={
-	'SECRET_KEY': #twilio secret key,
-	'SECRET_API_KEY':#twilio secret api key,
-	'MAIL_ID':#sender mail id,
-	'MAIL_PASSWORD':#sender mail password,
+	# 'SECRET_KEY': #twilio secret key,
+	# 'SECRET_API_KEY':#twilio secret api key,
+	# 'MAIL_ID':#sender mail id,
+	# 'MAIL_PASSWORD':#sender mail password,
 }
